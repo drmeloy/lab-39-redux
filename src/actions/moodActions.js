a72b06af507b22148cff9dda5ac24c626f4f09fb
drmeloy <drmeloy@gmail.com>
2020-02-13 13:38:51 -0700

    eat snack done

--- src/actions/moodActions.js
+++ src/actions/moodActions.js
@@ -1,4 +1,7 @@
 export const DRINK_COFFEE = 'DRINK_COFFEE';
-export const drinkCoffee = () => ({
-  type: DRINK_COFFEE
-});
+export const EAT_SNACK = 'EAT_SNACK';
+
+export const drinkCoffee = () => ({ type: DRINK_COFFEE });
+
+export const eatSnack = () => ({ type: EAT_SNACK });
+
--- src/actions/moodActions.test.js
+++ src/actions/moodActions.test.js
@@ -1,12 +1,13 @@
-import { DRINK_COFFEE, drinkCoffee } from './moodActions';
+import { DRINK_COFFEE, drinkCoffee, EAT_SNACK, eatSnack } from './moodActions';
 
 describe('mood actions', () => {
   it('can create a DRINK_COFFEE action', () => {
     const action = drinkCoffee();
-    expect(action).toEqual({
-      type: DRINK_COFFEE
-    });
+    expect(action).toEqual({ type: DRINK_COFFEE });
   });
 
-  it('can create a ')
+  it('can create a EAT_SNACK action', () => {
+    const action = eatSnack();
+    expect(action).toEqual({ type: EAT_SNACK });
+  });
 });
