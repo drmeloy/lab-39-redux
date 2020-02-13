d8c084180784747761ec8a73c2f0ee1b7eaf1acd
drmeloy <drmeloy@gmail.com>
2020-02-13 15:29:52 -0700

    added timer and reset button

--- src/containers/MoodsFn.js
+++ src/containers/MoodsFn.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import Face from '../components/face/Face';
 import { useDispatch, useSelector } from 'react-redux';
 import { drinkCoffee, eatSnack, takeNap, study } from '../actions/moodActions';
@@ -6,21 +7,42 @@ import { getSnacks, getNaps, getCoffees, getStudies } from '../selectors/moodSel
 import styles from './Controls.css';
 
 const MoodsFn = () => {
+  const [time, setTime] = useState(5);
+
   const dispatch = useDispatch();
   const coffees = useSelector(getCoffees);
   const snacks = useSelector(getSnacks);
   const naps = useSelector(getNaps);
   const studies = useSelector(getStudies);
 
+  useEffect(() => {
+    setInterval(() => {
+      setTime(time => time - 1);
+    }, 1000);
+  }, []);
+
   return (
     <>
-      <section className={styles.Controls}>
-        <button onClick={() => dispatch(drinkCoffee())}>coffee - {coffees}</button>
-        <button onClick={() => dispatch(eatSnack())}>snacks - {snacks}</button>
-        <button onClick={() => dispatch(takeNap())}>naps - {naps}</button>
-        <button onClick={() => dispatch(study())}>studies - {studies}</button>
-      </section>
-      <Face />
+      {time > 0 && 
+      <main>
+        <section className={styles.Controls}>
+          <button onClick={() => dispatch(drinkCoffee())}>coffee - {coffees}</button>
+          <button onClick={() => dispatch(eatSnack())}>snacks - {snacks}</button>
+          <button onClick={() => dispatch(takeNap())}>naps - {naps}</button>
+          <button onClick={() => dispatch(study())}>studies - {studies}</button>
+        </section>
+        <Face />
+        <h1 className={styles.Timer}>Time remaining: {time}</h1>
+      </main>
+      }
+      {time <= 0 && 
+      <main className={styles.Gameover}>
+        <h1>Time's up!</h1>
+        <Link to='/'>
+          <button>Facey face again?</button>
+        </Link>
+      </main>
+      }
     </>
   );
 }
