5af57dd813bed0b0c93a4d75fe7b4ae28be8e356
drmeloy <drmeloy@gmail.com>
2020-02-13 15:08:33 -0700

    refactor and css

--- src/containers/MoodsFn.js
+++ src/containers/MoodsFn.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import Controls from '../components/controls/Controls';
 import Face from '../components/face/Face';
 import { useDispatch, useSelector } from 'react-redux';
 import { drinkCoffee, eatSnack, takeNap, study } from '../actions/moodActions';
 import { getSnacks, getNaps, getCoffees, getStudies } from '../selectors/moodSelectors';
+import styles from './Controls.css';
 
 const MoodsFn = () => {
   const dispatch = useDispatch();
@@ -14,11 +14,12 @@ const MoodsFn = () => {
 
   return (
     <>
-      <Controls />
+      <section className={styles.Controls}>
         <button onClick={() => dispatch(drinkCoffee())}>coffee - {coffees}</button>
         <button onClick={() => dispatch(eatSnack())}>snacks - {snacks}</button>
         <button onClick={() => dispatch(takeNap())}>naps - {naps}</button>
         <button onClick={() => dispatch(study())}>studies - {studies}</button>
+      </section>
       <Face />
     </>
   );
